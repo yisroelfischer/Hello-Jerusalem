db7523c0deaf22816fa60b6b76bab7f9a0bcb1f5
yisroelfischer <ymfischer5@gmail.com>
2025-03-17 20:48:11 +0000

    tour component

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -7,7 +7,8 @@ import Tour from '../components/Tour'
 import './globals.css'
 
 export default function App (){
-    const [tour, setTour] = useState(null);
+    const [tourIndex, setTourIndex] = useState(0);
+    const [tours, setTours]= useState(null);
     const [state, setState] = useState(0);
 
     const enter  = (v) => {
@@ -16,13 +17,13 @@ export default function App (){
 
     const getTour = async (sites) => {
         try{
-            const url = `http://127.0.0.1:3001/get-tour?sites=${sites}`
+            const url = `http://127.0.0.1:5000/get-tour?sites=${sites}`
             const response = await fetch(url);
             if (!response.ok){
                 throw new Error(`HTTP error. Status: ${response.status}`)
             }
-            const tour = await response.json()
-            setTour(tour)
+            const res = await response.json()
+            setTours(res)
         } catch (error) {
             console.error('getTour failed');
             return null
@@ -35,19 +36,25 @@ export default function App (){
             console.log(`setting tour: ${v}`);
             getTour(v);
         }
+        if (state === 2){
+            if (tourIndex === tours.length()){
+                setState(0)
+            }
+            setTourIndex(prev => prev+1)
+        }
     }
 
     useEffect(() => {
-        if (tour){
+        if (tours){
             setState(2)
         }
-    },[tour]);
+    },[tours]);
     
     return(
         <main>
         {(state === 0) && <Home parentCallback={ (v) => enter(v) }/>}
         {(state === 1) && <New parentCallback={ (v) => handleClick(v) }/>}
-        {(state === 2) && <Tour tour={tour} parentCallback={ (v) => handleClick(v) }/>}
+        {(state === 2) && <Tour tour={tours[tourIndex]} parentCallback={ (v) => handleClick(v) }/>}
         </main>
     );
 }
--- frontend/components/New.js
+++ frontend/components/New.js
@@ -1,134 +1,144 @@
-import React,{ useState, useEffect } from 'react'
-import Chat from './Chat';
-import Error from './Error';
+import React, { useState, useEffect } from "react";
+import Chat from "./Chat";
+import Error from "./Error";
 
-function New({parentCallback}) {
-    console.log('hello')
-    const [picked, setPicked] = useState([]);
-    const [sites, setSites] = useState([]);
-    const [list, setList] = useState([])
+function New({ parentCallback }) {
+  console.log("hello");
+  const [picked, setPicked] = useState([]);
+  const [sites, setSites] = useState([]);
+  const [list, setList] = useState([]);
 
-
-    // Lists of sites organized by tag
-    const getSiteLists = async()=> {
-        console.log('getSiteLists')
-        try{
-           const response = (await fetch('http://127.0.0.1:5000/get-site-lists'));
-           if (!response.ok){
-            throw new Error(`HTTP error. Status: ${response.status}`)
-           }
-           const site_lists = await response.json();
-           return site_lists
-        }
-        catch(error){
-            console.error('getSiteLists failed');
-            return null
-        }
+  // Lists of sites organized by tag
+  const getSiteLists = async () => {
+    console.log("getSiteLists");
+    try {
+      const response = await fetch("http://127.0.0.1:5000/get-site-lists");
+      if (!response.ok) {
+        throw new Error(`HTTP error. Status: ${response.status}`);
+      }
+      const site_lists = await response.json();
+      return site_lists;
+    } catch (error) {
+      console.error("getSiteLists failed");
+      return null;
     }
+  };
 
-    useEffect(()=>{
-        console.log('useEffect')
-        const fetchSites = async()=> {
-            const siteLists = await getSiteLists();
-            if (siteLists){
-                setSites(siteLists);
-                setList(siteLists[0]);
-            }
-        }
-        fetchSites();
-    }, []);
-
-    const handleSubmit = (e)=> {
-        console.log('handleSubmit')
-        e.preventDefault();
-        console.log(`picked: ${picked}`)
-        parentCallback(picked);
-    }
-
-    const handleClick = (e)=> {
-        const value = e.target.value;
-        const tag = sites.find(item => item.tag === value);
-        if (tag){    
-            setList(tag)
-        }
-        else{
-            return (<><Error/></>)
-        }
-    }
-
-    useEffect(()=>{
-        // Style tab
-        const tabs = document.getElementsByClassName('tab');
-        for (let i = 0; i < tabs.length; i++){
-            const tab = tabs[i];
-            console.log(tab)
-            if (tab.id === list.tag){
-                tab.style.color = 'white';
-                tab.style.borderBottom = 'thick solid';
-            }else{
-                tab.style.color = 'inherit';
-                tab.style.borderBottom = 'thick solid var(--accent)'
-            }
-        }
-    },[list])
-
-    const handleChange = (e)=> {
-        console.log('handleChange')
-        setPicked((prevPicked)=> 
-            e.target.checked ? [...prevPicked, e.target.value] : prevPicked.filter(i => i !== e.target.value)
-        );
-        console.log(e.target.value)
+  useEffect(() => {
+    console.log("useEffect");
+    const fetchSites = async () => {
+      const siteLists = await getSiteLists();
+      if (siteLists) {
+        setSites(siteLists);
+        setList(siteLists[0]);
+      }
     };
+    fetchSites();
+  }, []);
 
+  const handleSubmit = (e) => {
+    console.log("handleSubmit");
+    e.preventDefault();
+    console.log(`picked: ${picked}`);
+    parentCallback(picked);
+  };
 
-    return(
-        <div className='container'>
-        <Chat context={`The user is on a page where they can select from a list of sites they want
+  const handleClick = (e) => {
+    const value = e.target.value;
+    const tag = sites.find((item) => item.tag === value);
+    if (tag) {
+      setList(tag);
+    } else {
+      return (
+        <>
+          <Error />
+        </>
+      );
+    }
+  };
+
+  useEffect(() => {
+    // Style tab
+    const tabs = document.getElementsByClassName("tab");
+    for (let i = 0; i < tabs.length; i++) {
+      const tab = tabs[i];
+      console.log(tab);
+      if (tab.id === list.tag) {
+        tab.style.color = "white";
+        tab.style.borderBottom = "thick solid";
+      } else {
+        tab.style.color = "inherit";
+        tab.style.borderBottom = "thick solid var(--accent)";
+      }
+    }
+  }, [list]);
+
+  const handleChange = (e) => {
+    console.log("handleChange");
+    setPicked((prevPicked) =>
+      e.target.checked
+        ? [...prevPicked, e.target.value]
+        : prevPicked.filter((i) => i !== e.target.value)
+    );
+    console.log(e.target.value);
+  };
+
+  return (
+    <div className="container">
+      <Chat
+        context={`The user is on a page where they can select from a list of sites they want
             to visit. When they click the Begin Tour button, the site will automatically calculate
             a tour visiting those sites from the videos in its database. The sites are arranged by
-            tags. The user can see the list of sites for each tag by clicking or tapping its tab`}/>
-        <form>
-            <div className='form-head'>
-                <p>Select the sites you want to visit and click Begin Tour</p>
-                <div className='tabs' id='tabs'>
-                    {sites.map((tag) =>
-                        <button  type='button'
-                        key={tag.tag} 
-                        className='tab' 
-                        id={tag.tag} 
-                        value={tag.tag} 
-                        onClick= { handleClick }>
-                            { tag.tag }
-                        </button>
-                        )}
-                </div>
-            </div>
-            <div className='form-main'>
-                {(list.list) && <div id='list' className='list'>
-                    {list.list.map((site) => 
-                    <div className='check-border'>
-                        <div key={site.id} className='check-container'>
-                            <input type='checkbox'
-                            className='check' 
-                            id={site.id} 
-                            value={site.id}
-                            onChange={ handleChange }>
-                            </input>
-                            <label htmlFor={site.id} className='check-label'>
-                                {site.name}
-                            </label>
-                        </div>
-                        </div>)
-                        }
-                </div>}
-            </div>
-            <div className='form-foot'>
-                <button className='button'
-                 onClick={handleSubmit}>Begin tour</button>
-            </div>      
-        </form>
+            tags. The user can see the list of sites for each tag by clicking or tapping its tab`}
+      />
+      <form>
+        <div className="form-head">
+          <p>Select the sites you want to visit and click Begin Tour</p>
+          <div className="tabs" id="tabs">
+            {sites.map((tag) => (
+              <button
+                type="button"
+                key={tag.tag}
+                className="tab"
+                id={tag.tag}
+                value={tag.tag}
+                onClick={handleClick}
+              >
+                {tag.tag}
+              </button>
+            ))}
+          </div>
         </div>
-    )
+        <div className="form-main">
+          {list.list && (
+            <div id="list" className="list">
+              {list.list.map((site) => (
+                <div className="check-border">
+                  <div key={site.id} className="check-container">
+                    <input
+                      type="checkbox"
+                      className="check"
+                      id={site.id}
+                      value={site.id}
+                      onChange={handleChange}
+                    ></input>
+                    <label htmlFor={site.id} className="check-label">
+                      {site.name}
+                    </label>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+        <div className="form-foot">
+          <button className="button" onClick={handleSubmit}>
+            Begin tour
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default New
+export default New;
